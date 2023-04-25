69ce5526e7cf253c99232b1b425cfcbd2d153bc1
Albert Mpepo <albertmpepo@gmail.com>
2023-04-25 14:58:56 -0700

    tasks complete

--- print_flags.c
+++ print_flags.c
@@ -10,25 +10,27 @@
 */
 int get_flags(const char *format, int *i)
 {
-    int j, curr_i;
-    int flags = 0;
-    const char flag_ch[] = {'+', '#', '0', '-', ' ', '\0'};
-    const int flag_array[] = {F_PLUS, F_HASH, F_ZERO, F_MINUS, F_SPACE, 0};
+	int j, curr_i;
+	int flags = 0;
+	const char flag_ch[] = {'+', '#', '0', '-', ' ', '\0'};
+	const int flag_array[] = {F_PLUS, F_HASH, F_ZERO, F_MINUS, F_SPACE, 0};
 
-    for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
-    {
-        for (j = 0; flag_ch[j] != '\0'; j++)
-        {
-            if (format[curr_i] == flag_ch[j])
-                flags |= flag_array[j];
-                break
-        }
+	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
+	{
+		for (j = 0; flag_ch[j] != '\0'; j++)
+		{
+			if (format[curr_i] == flag_ch[j])
+			{
+				flags |= flag_array[j];
+				break;
+			}
+		}
 
-        if (flags_ch[j] == 0)
-            break;
-    }
+		if (flag_ch[j] == 0)
+			break;
+	}
 
-    *i = curr_i - 1;
+	*i = curr_i - 1;
 
-    return (flags);
+	return (flags);
 }
