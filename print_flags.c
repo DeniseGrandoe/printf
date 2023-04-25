498dd7ce556f10204ee5e118fbcd7420f35e694f
Albert Mpepo <albertmpepo@gmail.com>
2023-04-25 22:19:42 +0200

    precision, flags, size, width

---
+++ print_flags.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * get_flags - determines the flags of printf.
+ *
+ * @format: Formatted string argument that will be printed.
+ * @i: pointer that ref arguments to be printed.
+ *
+ * Return: flags value
+*/
+int get_flags(const char *format, int *i)
+{
+    int j, curr_i;
+    int flags = 0;
+    const char flag_ch[] = {'+', '#', '0', '-', ' ', '\0'};
+    const int flag_array[] = {F_PLUS, F_HASH, F_ZERO, F_MINUS, F_SPACE, 0};
+
+    for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
+    {
+        for (j = 0; flag_ch[j] != '\0'; j++)
+        {
+            if (format[curr_i] == flag_ch[j])
+                flags |= flag_array[j];
+                break
+        }
+
+        if (flags_ch[j] == 0)
+            break;
+    }
+
+    *i = curr_i - 1;
+
+    return (flags);
+}
---
+++ print_precision.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * get_precision - determines the precision for printf.
+ *
+ * @format: Formatted string argument that will be printed.
+ * @i: pointer that ref arguments to be printed.
+ * @list: list of argument in printf.
+ *
+ * Return: precision value
+*/
+
+int get_precision(const char *format, int *i, va_list list)
+{
+    int curr_i = *i + 1;
+    int precision = -1;
+
+    if (format[curr_i] != '.')
+        return (precision);
+
+    for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
+    {
+        if (is_digit(format[curr_i]))
+        {
+            precision *= 10;
+            precision += format[curr_i] - '0';
+        }
+        else if (format[curr_i] == '*')
+        {
+            curr_i++;
+            precision = va_arg(list, int);
+            break;
+        }
+        else
+        {
+            break;
+        }
+    }
+    *i = curr_i -1;
+
+    return (precision);
+}
